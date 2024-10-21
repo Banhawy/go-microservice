51cd4ad6811cfee51a37a1dd1bede63896e680fd
Adham El Banhawy <the.benhawy@gmail.com>
2024-10-21 12:23:59 +0300

    wired the service

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/banhawy/go-microservices/internal/database"
+	"github.com/banhawy/go-microservices/internal/server"
+)
+
+func main() {
+	db, err := database.NewdatabaseClient()
+	if err != nil {
+		log.Fatalf("could not connect to database: %s", err)
+	}
+
+	srv := server.NewEchoServer(db)
+	if err := srv.Start(); err != nil {
+		log.Fatalf("could not start server: %s", err)
+	}
 }
